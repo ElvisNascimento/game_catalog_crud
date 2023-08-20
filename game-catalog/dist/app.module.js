5e714c91718a2995a95472a8efd6254901a1177a
Elvis Nascimento <elvisnascimentodev@gmail.com>
2023-08-20 09:00:10 -0300

    migrando para o typeorm 3.x

--- game-catalog/dist/app.module.js
+++ game-catalog/dist/app.module.js
@@ -12,13 +12,26 @@ const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const games_controller_1 = require("./games/games.controller");
 const games_module_1 = require("./games/games.module");
+const typeorm_1 = require("@nestjs/typeorm");
+const games_service_1 = require("./games/games.service");
 let AppModule = exports.AppModule = class AppModule {
 };
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [games_module_1.GamesModule],
+        imports: [games_module_1.GamesModule,
+            typeorm_1.TypeOrmModule.forRoot({
+                type: 'postgres',
+                host: 'localhost',
+                port: 5432,
+                username: 'postgres',
+                password: 'docker',
+                database: 'postgres',
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+        ],
         controllers: [app_controller_1.AppController, games_controller_1.GamesController],
-        providers: [app_service_1.AppService],
+        providers: [app_service_1.AppService, games_service_1.GamesService],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
--- game-catalog/src/app.module.ts
+++ game-catalog/src/app.module.ts
@@ -3,10 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GamesController } from './games/games.controller';
 import { GamesModule } from './games/games.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GamesService } from './games/games.service';
 
 @Module({
-  imports: [GamesModule],
+  imports: [GamesModule,
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port:5432,
+    username: 'postgres',
+    password: 'docker',
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+],
   controllers: [AppController, GamesController],
-  providers: [AppService],
+  providers: [AppService,GamesService],
 })
 export class AppModule {}
